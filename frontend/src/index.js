7454a02a2540f1afb7fd124b87d2fed8a9c7d2c2
ralon-1 <pravesh2008kumar@gmail.com>
2026-06-19 21:29:23 +0530

    Refactor index.js for improved component structure
    Reorganize imports and update component paths for consistency.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
+
 import HomePage from "./landing_page/home/HomePage";
 import Signup from "./landing_page/signup/Signup";
-import ProductPage from "./landing_page/product/ProductPage";
+import AboutPage from "./landing_page/about/AboutPage";
+import ProductPage from "./landing_page/products/ProductsPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
 import SupportPage from "./landing_page/support/SupportPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "/Users/pravesh/Downloads/THISPC/react_frontend/zerodha-clone/frontend/src/Navbar.js";
-import Footer from "/Users/pravesh/Downloads/THISPC/react_frontend/zerodha-clone/frontend/src/Footer.js";
-import AboutPage from "./landing_page/about/AboutPage";
-import NotFound from "/Users/pravesh/Downloads/THISPC/react_frontend/zerodha-clone/frontend/src/NotFound.js";
+
+import NotFound from "./landing_page/NotFound";
+import Navbar from "./landing_page/Navbar";
+import Footer from "./landing_page/Footer";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
