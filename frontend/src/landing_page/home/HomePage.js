e2f64e637b9dc9a661a35467a99cd60ba3d87600
ralon-1 <pravesh2008kumar@gmail.com>
2026-06-19 21:27:46 +0530

    Refactor imports in HomePage.js to relative paths
    Updated import paths for components in HomePage.js to use relative paths.

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Awards from '/Users/pravesh/Downloads/THISPC/react_frontend/zerodha-clone/frontend/src/landing_page/home/Awards.js';
-import Education from '/Users/pravesh/Downloads/THISPC/react_frontend/zerodha-clone/frontend/src/landing_page/home/Education.js';
-import Stats from './Stats';
-import OpenAccount from '/Users/pravesh/Downloads/THISPC/react_frontend/zerodha-clone/frontend/src/OpenAccount.js';
-// import Navbar from '/Users/pravesh/Downloads/THISPC/react_frontend/zerodha-clone/frontend/src/Navbar.js';
-import Hero from './Hero';
-import Pricing from './Pricing';
-import Footer from '/Users/pravesh/Downloads/THISPC/react_frontend/zerodha-clone/frontend/src/Footer.js'
+import React from "react";
+import Hero from "./Hero";
+import Awards from "./Awards";
+import Stats from "./Stats";
+import Pricing from "./Pricing";
+import Education from "./Education";
+
+import OpenAccount from "../OpenAccount";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+
 function HomePage() {
     return (  
         <>
