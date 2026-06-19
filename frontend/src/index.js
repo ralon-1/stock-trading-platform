e25a3e58cd50fef88a3657a1a96b65af68465b2d
ralon-1 <pravesh2008kumar@gmail.com>
2026-06-19 21:38:31 +0530

    Refactor imports and update routing structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
+
 import HomePage from "./landing_page/home/HomePage";
 import Signup from "./landing_page/signup/Signup";
-import ProductPage from "./landing_page/product/ProductPage";
+import AboutPage from "./landing_page/about/AboutPage";
+import ProductPage from "./landing_page/products/ProductsPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
 import SupportPage from "./landing_page/support/SupportPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./Navbar.js";
-import Footer from "./Footer.js";
-import AboutPage from "./landing_page/about/AboutPage";
-import NotFound from "./NotFound";
+
+import NotFound from "./landing_page/NotFound";
+import Navbar from "./landing_page/Navbar";
+import Footer from "./landing_page/Footer";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
