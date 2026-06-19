f0cef38ed0f2e954608024d2039d6728544589fa
ralon-1 <pravesh2008kumar@gmail.com>
2026-06-19 21:18:05 +0530

    Comment out Navbar import in HomePage.js
    Comment out the Navbar import in HomePage.js

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -3,7 +3,7 @@ import Awards from '/Users/pravesh/Downloads/THISPC/react_frontend/zerodha-clone
 import Education from '/Users/pravesh/Downloads/THISPC/react_frontend/zerodha-clone/frontend/src/landing_page/home/Education.js';
 import Stats from './Stats';
 import OpenAccount from '/Users/pravesh/Downloads/THISPC/react_frontend/zerodha-clone/frontend/src/OpenAccount.js';
-import Navbar from '/Users/pravesh/Downloads/THISPC/react_frontend/zerodha-clone/frontend/src/Navbar.js';
+// import Navbar from '/Users/pravesh/Downloads/THISPC/react_frontend/zerodha-clone/frontend/src/Navbar.js';
 import Hero from './Hero';
 import Pricing from './Pricing';
 import Footer from '/Users/pravesh/Downloads/THISPC/react_frontend/zerodha-clone/frontend/src/Footer.js'
